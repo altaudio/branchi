c08288c41223be90fa0392da43b54949c5083c11
Alex Smith <altaudiolabs@gmail.com>
2021-12-29 08:45:48 +0000

    Lists git branches

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,11 +1,37 @@
-import React, {FC} from 'react';
-import {Text} from 'ink';
+import { exec } from "child_process";
+import React, { FC, useEffect, useState } from "react";
+import { Text } from "ink";
 
-const App: FC<{name?: string}> = ({name = 'Stranger'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
-);
+const App: FC<{ name?: string }> = () => {
+	const [branches, setBranches] = useState<string[]>([]);
+
+	useEffect(() => {
+		exec("git branch", (error, stdout, stderr) => {
+			if (error) {
+				throw error;
+			}
+			if (stderr) {
+				throw stderr;
+			}
+
+			const parsedBranches = stdout
+				.replace("*", "")
+				.split("\n")
+				.map((branch) => branch.replace(/\s/g, ""))
+				.sort();
+
+			setBranches(parsedBranches);
+		});
+	}, []);
+
+	return (
+		<>
+			{branches.map((branch) => (
+				<Text key={branch}>{branch}</Text>
+			))}
+		</>
+	);
+};
 
 module.exports = App;
 export default App;
