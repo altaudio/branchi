31d121754d15cfe784816a84169e05772b356828
Alex Smith <altaudiolabs@gmail.com>
2021-12-29 08:56:38 +0000

    Adds styling for current branch navigation

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,9 +1,10 @@
 import { exec } from "child_process";
 import React, { FC, useEffect, useState } from "react";
-import { Text } from "ink";
+import { Text, Box } from "ink";
 
 const App: FC<{ name?: string }> = () => {
 	const [branches, setBranches] = useState<string[]>([]);
+	const [currentBranch, setCurrentBranch] = useState<string | null>(null);
 
 	useEffect(() => {
 		exec("git branch", (error, stdout, stderr) => {
@@ -18,17 +19,36 @@ const App: FC<{ name?: string }> = () => {
 				.replace("*", "")
 				.split("\n")
 				.map((branch) => branch.replace(/\s/g, ""))
+				.filter((branch) => !!branch)
 				.sort();
 
 			setBranches(parsedBranches);
+
+			const firstBranch = parsedBranches[0];
+
+			if (!firstBranch) {
+				throw new Error("No first branch found!");
+			}
+
+			setCurrentBranch(firstBranch);
 		});
 	}, []);
 
 	return (
 		<>
-			{branches.map((branch) => (
-				<Text key={branch}>{branch}</Text>
-			))}
+			{branches.map((branch) => {
+				const isCurrentBranch = branch === currentBranch;
+				return (
+					<Box key={branch}>
+						{isCurrentBranch ? (
+							<Text color="yellow">&#62; </Text>
+						) : (
+							<Text>&nbsp; </Text>
+						)}
+						<Text color={isCurrentBranch ? "yellow" : "blue"}>{branch}</Text>
+					</Box>
+				);
+			})}
 		</>
 	);
 };
